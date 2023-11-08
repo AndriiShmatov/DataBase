fcccc8e512e78a72e64692f12a07ee580285afd0
Andrew <128030123+AndriiShmatov@users.noreply.github.com>
2023-11-08 09:14:20 +0200

    Delete lab6/country.sql

--- lab6/country.sql
+++
@@ -1,9 +0,0 @@
-create table country
-(
-    id           int  not null
-        primary key,
-    name         text null,
-    country_code text null
-);
-
-INSERT INTO labs.country (id, name, country_code) VALUES (1, 'Ukraine', 'UA');
