6c3941afbdbd4e14a7896f4fcc2233ba0bdefeb8
Andrew <128030123+AndriiShmatov@users.noreply.github.com>
2023-11-08 09:14:02 +0200

    Delete lab6/city_metro.sql

--- lab6/city_metro.sql
+++
@@ -1,8 +0,0 @@
-create table city_metro
-(
-    id        int  not null Primary key,
-    Name_city text null,
-    id_city   int  null
-);
-
-INSERT INTO labs.city_metro (id, Name_city, id_city) VALUES (1, 'Kharkiv', 1);
