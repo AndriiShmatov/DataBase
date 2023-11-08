c43408d35b5ba0a6b380d5b73f7bc49b19334133
Andrew <128030123+AndriiShmatov@users.noreply.github.com>
2023-11-08 09:18:17 +0200

    Update city_metro.sql

--- lab6/city_metro.sql
+++ lab6/city_metro.sql
@@ -1,8 +1,8 @@
 create table city
 (
-    id        int  null Primary Key,
-    Name_city text null,
-    id_city   int  null
+    id        int not null Primary Key,
+    Name_city text not null,
+    id_city   int  not null
 );
 
 INSERT INTO labs.city (id, Name_city, id_city) VALUES (1, 'Kharkiv', 1);
@@ -10,9 +10,9 @@ INSERT INTO labs.city (id, Name_city, id_city) VALUES (1, 'Kharkiv', 1);
 
 create table lines
 (
-    id        int  null Primary Key,
-    Line_name text null,
-    id_city   int  null
+    id        int  not null Primary Key,
+    Line_name text not null,
+    id_city   int  not null
 );
 
 INSERT INTO labs.lines (id, Line_name, id_city) VALUES (1, 'Saltivska', 1);
@@ -20,10 +20,9 @@ INSERT INTO labs.lines (id, Line_name, id_city) VALUES (1, 'Saltivska', 1);
 
 create table country
 (
-    id           int  not null
-        primary key,
-    name         text null Primary Key,
-    country_code text null
+    id  int  not null primary key,
+    name text not null,
+    country_code text not null
 );
 
 INSERT INTO labs.country (id, name, country_code) VALUES (1, 'Ukraine', 'UA');
@@ -31,18 +30,18 @@ INSERT INTO labs.country (id, name, country_code) VALUES (1, 'Ukraine', 'UA');
 
 create table connection_station
 (
-    id          int nullPrimary Key,
-    id_station  int null,
-    id_station2 int null
+    id          int not null Primary Key,
+    id_station  int not null,
+    id_station2 int not null
 );
 
 INSERT INTO labs.connection_station (id, id_station, id_station2) VALUES (1, 1, 2);
 
 create table stations
 (
-    id            int  null Primary Key,
-    Name_station  text null,
-    id_metro_line int  null
+    id            int  not null Primary Key,
+    Name_station  text not null,
+    id_metro_line int  not null
 );
 
 INSERT INTO `stations` (`id`, `Name_station`, `id_metro_line`) VALUES
