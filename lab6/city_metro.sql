afd0677d86a5295e25abb8109b1a3e751fed6a99
Andrew <128030123+AndriiShmatov@users.noreply.github.com>
2023-11-08 09:15:33 +0200

    new tables

---
+++ lab6/city_metro.sql
@@ -0,0 +1,51 @@
+create table city_metro
+(
+    id        int  null Primary Key,
+    Name_city text null,
+    id_city   int  null
+);
+
+INSERT INTO labs.city_metro (id, Name_city, id_city) VALUES (1, 'Kharkiv', 1);
+
+
+create table metro_lines
+(
+    id        int  null Primary Key,
+    Line_name text null,
+    id_city   int  null
+);
+
+INSERT INTO labs.metro_lines (id, Line_name, id_city) VALUES (1, 'Saltivska', 1);
+
+
+create table country
+(
+    id           int  not null
+        primary key,
+    name         text null Primary Key,
+    country_code text null
+);
+
+INSERT INTO labs.country (id, name, country_code) VALUES (1, 'Ukraine', 'UA');
+
+
+create table connection_station
+(
+    id          int nullPrimary Key,
+    id_station  int null,
+    id_station2 int null
+);
+
+INSERT INTO labs.connection_station (id, id_station, id_station2) VALUES (1, 1, 2);
+
+create table metro_stations
+(
+    id            int  null Primary Key,
+    Name_station  text null,
+    id_metro_line int  null
+);
+
+INSERT INTO `stations` (`id`, `Name_station`, `id_metro_line`) VALUES
+(2, 'Heroes of Labor', 1),
+(1, 'Academician Pavlova', 1),
+(3, 'Studenska', 1);
