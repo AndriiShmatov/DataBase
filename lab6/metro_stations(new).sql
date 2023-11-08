a80a90acc465108983a5fa7dae502d56a1daf75b
Andrew <128030123+AndriiShmatov@users.noreply.github.com>
2023-11-08 09:14:47 +0200

    Delete lab6/metro_stations(new).sql

--- lab6/metro_stations(new).sql
+++
@@ -1,6 +0,0 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-
-INSERT INTO `stations` (`id`, `Name_station`, `id_metro_line`) VALUES
-(2, 'Heroes of Labor', 1),
-(1, 'Academician Pavlova', 1),
-(3, 'Studenska', 1);
