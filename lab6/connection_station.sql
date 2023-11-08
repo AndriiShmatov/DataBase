f569e8103d787c94f58f8dcbdc835cdf8539fad4
Andrew <128030123+AndriiShmatov@users.noreply.github.com>
2023-11-08 09:14:29 +0200

    Delete lab6/connection_station.sql

--- lab6/connection_station.sql
+++
@@ -1,8 +0,0 @@
-create table connection_station
-(
-    id          int not null Primary key,
-    id_station  int null,
-    id_station2 int null
-);
-
-INSERT INTO labs.connection_station (id, id_station, id_station2) VALUES (1, 1, 2);
