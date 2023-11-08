5026a60968a4527af4bc1cedb01fe19f61b0d9a4
Andrew <128030123+AndriiShmatov@users.noreply.github.com>
2023-11-08 09:14:36 +0200

    Delete lab6/metro_lines.sql

--- lab6/metro_lines.sql
+++
@@ -1,8 +0,0 @@
-create table lines
-(
-    id        int  not null Primary key,
-    Line_name text null,
-    id_city   int  null
-);
-
-INSERT INTO labs.lines (id, Line_name, id_city) VALUES (1, 'Saltivska', 1);
